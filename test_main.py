800eb338bed61223a54e9583fc61525e6700c7e8
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-09-24 21:22:36 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -21,41 +21,7 @@ def test_calculate_statistics():
     # assert result['mean']['danceability'] == 0.1
     # assert result['median']['danceability'] == 0.1
 
-# Define test cases for visualize_data function
-def test_visualize_data():
-    # Create a sample CSV file for testing
-    sample_data = pl.DataFrame({
-        'danceability': [0.7, 0.6, 0.8],
-        'energy': [0.8, 0.7, 0.9],
-        'artist_popularity': [80, 70, 90],
-        'loudness': [-5, -6, -4]
-    })
-    sample_data.write_csv('test_data.csv')
 
-
-    # Test the visualize_data function
-    result = main.visualize_data('test_data.csv')
-
-    # You can add assertions here to check the expected behavior
-
-# Define test cases for calculate_correlation function
-def test_calculate_correlation():
-    # Create a sample CSV file for testing
-    sample_data = pl.DataFrame({
-        'danceability': [0.7, 0.6, 0.8],
-        'energy': [0.8, 0.7, 0.9],
-        'artist_popularity': [80, 70, 90],
-        'loudness': [-5, -6, -4]
-    })
-    sample_data.write_csv('test_data.csv')
-
-
-    # Test the calculate_correlation function
-    result = main.calculate_correlation('test_data.csv')
-
-    # You can add assertions here to check the expected behavior
-
-# Clean up the test file by removing the sample data file
 def teardown():
     import os
     if os.path.exists('test_data.csv'):
